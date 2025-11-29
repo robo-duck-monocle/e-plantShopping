b6d7cc62040d5f60bfad06d40db449e64afc52b5
robo-duck-monocle <kp3rd.007@gmail.com>
2025-11-28 20:24:53 -0500

    corrected CartItems and the UI states as expressed in Task 3

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -19,7 +19,7 @@ export const CartSlice = createSlice({
             }
         },
         removeItem: (state, action) => {
-            state.items = state.items.filter(item => item.name !== action.payload);
+            state.items = state.items.filter(item => item.name !== action.payload.name);
         },
         updateQuantity: (state, action) => {
             const { name, quantity } = action.payload; // Destructure the product name and new quantity from the action payload
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -7,6 +7,7 @@ function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const cartItems = useSelector((state) => state.cart);
     const dispatch = useDispatch();
 
     const plantsArray = [
@@ -261,8 +262,23 @@ function ProductList({ onHomeClick }) {
         dispatch(addItem(item));
         setAddedToCart((prevState) => ({
             ...prevState, [item.name]: true,
-        }))
+        }));
     };
+
+    const getTotalCartItems = () => {
+        let totalItems = 0;
+        cartItems.items.forEach(item => {
+            totalItems += item.quantity;
+        });
+
+        return totalItems;
+    }
+
+    const plantInCart = (plantName) => {
+        return cartItems.items.some((i) => i.name === plantName);
+    }
+
+    const numItems = getTotalCartItems();
 
 
     return (
@@ -282,7 +298,21 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div>
+                        <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                            <h1 className='cart'>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                                    <rect width="156" height="156" fill="none"></rect>
+                                    <text fill="#ffffff" font-size="90" font-family="Verdana" x="100" y="150">
+                                        {numItems > 0 ? `${numItems}` : ''}
+                                    </text>
+                                    <circle cx="80" cy="216" r="12"></circle>
+                                    <circle cx="184" cy="216" r="12"></circle>
+                                    <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                                </svg>
+                            </h1>
+                        </a>
+                    </div>
                 </div>
             </div>
             {!showCart ? (
@@ -304,12 +334,18 @@ function ProductList({ onHomeClick }) {
                                         {/* Display other plant details like description and cost */}
                                         <div className="product-description">{plant.description}</div> {/* Display plant description */}
                                         <div className="product-cost">${plant.cost}</div> {/* Display plant cost */}
-                                        <button
-                                            className="product-button"
-                                            onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
-                                        >
-                                            Add to Cart
-                                        </button>
+                                        {plantInCart(plant.name) ?
+                                            < button className="product-button added-to-cart">
+                                                Added to Cart
+                                            </button>
+                                            :
+                                            <button
+                                                className="product-button"
+                                                onClick={() => handleAddToCart(plant)} // Handle add plant to cart
+                                            >
+                                                Add to Cart
+                                            </button>
+                                        }
                                     </div>
                                 ))}
                             </div>
@@ -318,8 +354,9 @@ function ProductList({ onHomeClick }) {
                 </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
-            )}
-        </div>
+            )
+            }
+        </div >
     );
 }
 
